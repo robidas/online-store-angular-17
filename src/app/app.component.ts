5559da967e0fd30beca2611ba82bee33f9a54346
Rob Das <noreply@dev3>
2024-06-15 18:19:38 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainLayoutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'start', component: StartPageComponent },
+    { path: 'stuff', component: ProductListComponent },
+    { path: '', redirectTo: 'start', pathMatch: 'full' }
+  ];
