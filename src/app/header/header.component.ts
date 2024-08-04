c7f57411da24f8a0a28b1dca13b1526912c6946d
Rob Das <noreply@dev3>
2024-08-03 20:31:59 -0600

    CommonModule removed from header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router'; 
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
@@ -7,7 +6,7 @@ import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
